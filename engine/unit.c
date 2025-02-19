adf80a1132ddac1531030718e7806649535cd294
ChrisBrinchLarsen <chrisbrinchlarsen@gmail.com>
2025-02-19 13:16:46 +0100

    Makefile for unit tests

--- engine/unit.c
+++ engine/unit.c
@@ -8,5 +8,5 @@ int main() {
     cache_rd_b(memory, L1, 0b10000100000010000000010000001000);
     cache_rd_b(memory, L1, 0b0000100000001000001000000010000);
     cache_rd_b(memory, L1, 0b00010000000001000000100010000100);
-    printCache(L1);
+    PrintCache(L1);
 }
