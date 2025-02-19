6367f44886edd4ad45f4c7f7327f8e7c929f6259
ChrisBrinchLarsen <chrisbrinchlarsen@gmail.com>
2025-02-19 11:31:25 +0100

    Started unit testing file

---
+++ engine/unit.c
@@ -0,0 +1,12 @@
+#include "cache.h"
+#include "memory.h"
+
+int main() {
+    struct memory* memory = memory_create();
+    Cache_t** caches = ParseCPUArchitecture("./Architectures/SimpleCPU.md");
+    Cache_t* L1 = caches[0];
+    cache_rd_b(memory, L1, 0b10000100000010000000010000001000);
+    cache_rd_b(memory, L1, 0b0000100000001000001000000010000);
+    cache_rd_b(memory, L1, 0b00010000000001000000100010000100);
+    printCache(L1);
+}
