47f60b7749cd8563746e4c079360455c6cbffba9
ChrisBrinchLarsen <chrisbrinchlarsen@gmail.com>
2025-02-26 16:21:17 +0100

    Modified matrix size to be smaller

--- engine/testing/C_Programs/matrix_good.c
+++ engine/testing/C_Programs/matrix_good.c
@@ -2,17 +2,17 @@
 
 int main(int argc, char* argv[]) {
   char buffer[20];
-  int matrix[5000][5000];
+  int matrix[100][100];
   
-  for (int i = 0; i < 5000; i++) {
-    for (int j = 0; j < 5000; j++) {
+  for (int i = 0; i < 100; i++) {
+    for (int j = 0; j < 100; j++) {
       matrix[i][j] = 1;
     }
   }
 
   unsigned int sum = 0;
-  for (int i = 0; i < 5000; i++) {
-    for (int j = 0; j < 5000; j++) {
+  for (int i = 0; i < 100; i++) {
+    for (int j = 0; j < 100; j++) {
       sum += matrix[i][j];
     }
   }
