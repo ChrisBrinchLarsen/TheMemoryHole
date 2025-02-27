e7db680284fba804e60bcb050460f667a0e72ad4
ChrisBrinchLarsen <chrisbrinchlarsen@gmail.com>
2025-02-27 12:00:19 +0100

    Website can take program input and it gets compiled and executed on the backend

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import os
 import logging
 import uuid
 import subprocess
@@ -13,7 +14,7 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('visualizer.html')
+    return render_template('index.html')
 
 # TODO: Very important that we make the config and program filenames be unique
 # and random, as well as have them cleaned ud (deleted) after use. This is due
@@ -23,6 +24,10 @@ def index():
 def handle_run_program(data):
     config = data["config"]
     program = data["program"]
+    id = uuid.uuid4()
+    architecture_file_name = f"architecture_{id}"
+    program_file_name = f"program_{id}"
+    architecture_file_name = "./engine/testing/Architectures/SimpleCPU.md"
 
     # with open("config", "w") as file:
     #     file.write(f"{config["addr_len"]}\n")
@@ -35,23 +40,36 @@ def handle_run_program(data):
     #         file.write(f"{config["caches"][i]["size"]}\n")
     #         file.write(f"{config["caches"][i]["associativity"]}\n")
 
-    with open("config", "w") as file:
-        file.write(f"32\n")
-        file.write(f"1\n")
-        file.write(f"8\n")
-        cache_levels: int = 2
-        file.write(f"{cache_levels}\n")
-        file.write(f"L1\n")
-        file.write(f"128\n")
-        file.write(f"2\n")
-        file.write(f"L2\n")
-        file.write(f"256\n")
-        file.write(f"4\n")
+    # with open(architecture_file_name, "w") as file:
+    #     file.write(f"32\n")
+    #     file.write(f"1\n")
+    #     file.write(f"8\n")
+    #     cache_levels: int = 2
+    #     file.write(f"{cache_levels}\n")
+    #     file.write(f"L1\n")
+    #     file.write(f"128\n")
+    #     file.write(f"2\n")
+    #     file.write(f"L2\n")
+    #     file.write(f"256\n")
+    #     file.write(f"4\n")
 
-    with open("program", 'w') as file: file.write(program)
-    result = subprocess.run([f"./Engine/prototype.exe", "./config", "./program"], capture_output=True, text=True)
-    print("C program finished:\n")
+    with open(f"{program_file_name}.c", 'w') as file: file.write(program)
+
+    # Compile from C -> RISC-V
+    os.system(f"./riscv/bin/riscv32-unknown-elf-gcc -march=rv32im -mabi=ilp32 -fno-tree-loop-distribute-patterns -mno-relax -O1 {program_file_name}.c lib.c -static -nostartfiles -nostdlib -o {program_file_name}.riscv")
+    #subprocess.run(["./riscv/bin/riscv32-unknown-elf-gcc", "-march=rv32im", "-mabi=ilp32", "-fno-tree-loop-distribute-patterns", "-mno-relax", "-O1", f"{program_file_name}.c", "lib.c", "-static", "-nostartfiles", "-nostdlib", "-o", f"{program_file_name}.riscv"])
+    # Compile from RISC-V -> dis
+    os.system(f"./riscv/bin/riscv32-unknown-elf-objdump -s -w {program_file_name}.riscv > {program_file_name}.dis")
+    #subprocess.run(["./riscv/bin/riscv32-unknown-elf-objdump", "-s", "-w", f"{program_file_name}.riscv", ">", f"{program_file_name}.dis"])
+    os.system(f"./riscv/bin/riscv32-unknown-elf-objdump -S {program_file_name}.riscv >> {program_file_name}.dis")
+
+    args = "20"
+    result = subprocess.run(["./engine/sim", architecture_file_name, f"{program_file_name}.dis", "--", f"{args}"], capture_output=True, text=True)
     print(result.stdout)
+
+
+    #result = subprocess.run([f"./Engine/prototype.exe", "./config", "./program"], capture_output=True, text=True)
+    os.system(f"rm -f accesses cache_log {program_file_name}.riscv {program_file_name}.dis {program_file_name}.c")
 
 
 
