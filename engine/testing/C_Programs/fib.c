731856d96f242845281f41aec6e2b853e8a78adc
ChrisBrinchLarsen <chrisbrinchlarsen@gmail.com>
2025-03-05 12:15:33 +0100

    Added argument passing

--- engine/testing/C_Programs/fib.c
+++ engine/testing/C_Programs/fib.c
@@ -11,14 +11,18 @@ int main(int argc, char* argv[]) {
     terminate(1);
   }
   char buffer[20];
-  unsigned int arg = str_to_uns(argv[1]);
-  print_string("fib(");
-  print_string(argv[1]);
-  print_string(") = ");
-  unsigned int res = fib(arg);
-  uns_to_str(buffer, res);
-  print_string(buffer);
-  print_string("\n");
+  unsigned int arg;
+  unsigned int res;
+  for (int i = 1; i < argc; i++) {
+    arg = str_to_uns(argv[i]);
+    print_string("fib(");
+    print_string(argv[i]);
+    print_string(") = ");
+    res = fib(arg);
+    uns_to_str(buffer, res);
+    print_string(buffer);
+    print_string("\n");
+  }
   return 0;
 }
 
